f35404c97f197633adafdfe8edb1fdd2ef53871f
Marten Schiwek <marten.schiwek@sap.com>
2025-08-06 23:45:21 +0200

    First try

---
+++ ams/dcl/cap/basePolicies.dcl
@@ -0,0 +1,16 @@
+// ---------------------------------HEADER_START-----------------------------------------------
+// Generated from a CAP model by the SAP AMS Plugin (@sap/ams) 3.3.0
+// hash of generated content: ffbd527088a2eebce80111113fe1f136cfcaea722f4e7b298c6e9bffa63ba9ec
+// ----------------------------------HEADER_END------------------------------------------------
+
+POLICY "admin" {
+	ASSIGN ROLE "admin";
+}
+
+POLICY "processor" {
+	ASSIGN ROLE "processor";
+}
+
+POLICY "reviewer" {
+	ASSIGN ROLE "reviewer";
+}
--- server.ts
+++ server.ts
@@ -1,33 +1,81 @@
-const cds = require('@sap/cds');
-const express = require('express');
-const path = require('path');
-const xsenv = require('@sap/xsenv');
-const passport = require('passport');
-const { JWTStrategy } = require('@sap/xssec').v3;
+import cds from '@sap/cds';
+import express from 'express';
+import path from 'path';
 
-if (process.env.NODE_ENV === 'production') {
-    cds.once('bootstrap', (app) => {
-      app.use(
-        '/i18n',
-        passport.initialize(),
-        passport.authenticate(new JWTStrategy(xsenv.getServices({ xsuaa: { tag: 'xsuaa' } }).xsuaa), {
-          session: false,
-          failWithError: true,
-        }),
-        express.static(path.join(__dirname, '_i18n'), {
-            maxAge: 31536000
-        }),
-      );
-    });
-} else {
-    cds.once('bootstrap', (app) => {
-        app.use(
-          '/i18n',
-          express.static(path.join(__dirname, '_i18n'), {
-            maxAge: 31536000
-        }),
-        );
-      });
+import { SECURITY_CONTEXT } from '@sap/xssec';
+import { 
+  createSecurityContext, IdentityService, 
+  IdentityServiceToken, IdentityServiceSecurityContext, errors
+} from '@sap/xssec';
+
+// --- Sets up AUTHENTICATION as pre-condition for AUTHORIZATION ---
+
+const authenticate = process.env.NODE_ENV === 'test' ? buildMockAuthMiddleware() : buildAuthMiddleware();
+
+/*
+Middleware for SAP Identity Service based authentication 
+following the example in the @sap/xssec documentation:
+https://www.npmjs.com/package/@sap/xssec#example
+*/
+function buildAuthMiddleware() {
+    const identityService = require('./identityService');
+
+    return async function authenticate(req, res, next) {
+        try {
+            const secContext = await createSecurityContext(identityService, { req });
+            req[SECURITY_CONTEXT] = secContext;
+            return next();
+        } catch (e) {
+            if (e instanceof errors.ValidationError) {
+                console.error("Unauthenticated request: ", e);
+                return res.sendStatus(401);
+            }
+
+            throw e; // handled in express error handler
+        }
+    }
 }
+
+/*
+Middleware for mocked authentication 
+following the recommendation in the @sap/xssec documentation for testing without real JWTs:
+https://www.npmjs.com/package/@sap/xssec#testing
+*/
+function buildMockAuthMiddleware() {
+    return async function mockAuthentication(req, res, next) {
+        const basicAuthUser = req.headers['authorization']?.split(' ')[1];
+        const user = Buffer.from(basicAuthUser, 'base64').toString().split(':')[0];
+        const [username, api] = user.split('|');
+
+        if (!username) {
+            return res.sendStatus(401);
+        }
+
+        const mockPayload = {
+            app_tid: "default",
+            scim_id: username,
+            sub: username,
+            azp: "client_id",
+            ias_apis: api ? [api] : []
+        };
+
+        const mockToken = new IdentityServiceToken(null, { header: {}, payload: mockPayload });
+        const mockService = new IdentityService({});
+        const mockedSecurityContext = new IdentityServiceSecurityContext(mockService, mockToken, {});
+
+        req[SECURITY_CONTEXT] = mockedSecurityContext;
+        next();
+    }
+}
+
+cds.once('bootstrap', (app) => {
+  app.use(
+    '/i18n',
+    authenticate,
+    express.static(path.join(__dirname, '_i18n'), {
+        maxAge: 31536000
+    }),
+  );
+});
 
 module.exports = cds.server;
